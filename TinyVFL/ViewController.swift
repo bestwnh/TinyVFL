eeb0d144331ee74c340c1bbbf7e3f6b2d0000c20
Galvin <b02437@gmail.com>
2019-01-18 14:19:16 +0800

    init basic project

---
+++ TinyVFL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinyVFL
+//
+//  Created by Galvin on 2019/1/18.
+//  Copyright © 2019 @GalvinLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
